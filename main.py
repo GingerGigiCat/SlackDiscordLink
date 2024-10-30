096eb19bb9b8225d26c627654b71fe4b69e59198
gigi <cat@gigicat.co.uk>
2024-10-30 22:33:14 +0000

    yay it works discord to slack but not slack to discord yet

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
 import os
-
 import discord
 from discord.ext import commands
 import slack_sdk
 from slackeventsapi import SlackEventAdapter
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-SLACK_SIGNING_SECRET = open("slack_token", "r").read()
+import threading
 
+sclient = WebClient(token=open("slack_token", "r").read())
+
+SLACK_SIGNING_SECRET = open("slack_signing_secret", "r").read()
 slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, endpoint="/slack/events")
-@slack_events_adapter.on(
+
+def get_channel_name(channel_id):
+    try:
+        response = sclient.conversations_info(channel=channel_id)
+        channel_name = response['channel']['name']
+        return channel_name
+    except SlackApiError as e:
+        print(f"Error fetching channel info: {e.response['error']}")
+        return None
+
+@slack_events_adapter.on("reaction_added")
+def reaction_added(event_data):
+    emoji = event_data["event"]["reaction"]
+    print(emoji)
+
+@slack_events_adapter.on("message")
+def handle_message(event_data):
+    message = event_data["event"]
+    # If the incoming message contains "hi", then respond with a "Hello" message
+    if message.get("subtype") is None and "hi458" in message.get('text'):
+        channel = message["channel"]
+        print(channel)
+        message = "Hello <@%s>! :tada:" % message["user"]
+        sclient.chat_postMessage(channel=channel, text=message)
+
+class DisClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        sclient.chat_postMessage(channel="#bot-spam", text=message.content, username=message.author.display_name, icon_url=message.author.avatar.url)
+
+
+
+#slack_events_adapter.start(port=3000)
+slack_thread = threading.Thread(target=slack_events_adapter.start, kwargs={'port': 3000})
+slack_thread.start()
+
+intents = discord.Intents.all()
+intents.message_content = True
+dclient = DisClient(intents=intents)
+dclient.run(open("discord_token", "r").read())
